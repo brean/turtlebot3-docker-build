eac15167ced3b13b910ebbf635018132fd048b87
Andreas Bresser <Andreas.Bresser@dfki.de>
2026-06-23 11:59:59 +0200

    Update build.bash
    force arm64 as architecture

--- build.bash
+++ build.bash
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 # Set the name of the builder
-BUILDER_NAME="turtlebuilder"
+BUILDER_NAME="turtlebotbuilder"
 
 # Check if the builder already exists
 if docker buildx inspect "$BUILDER_NAME" > /dev/null 2>&1; then
     echo "Builder '$BUILDER_NAME' already exists."
+    docker buildx use --name "$BUILDER_NAME"
 else
     echo "Builder '$BUILDER_NAME' does not exist. Creating a new one..."
     
     # Create a new builder with the docker-container driver
-    docker buildx create --name "$BUILDER_NAME" --use --driver docker-container
+    docker buildx create --name "$BUILDER_NAME" --use --platform arm64 --driver docker-container
     
     # Inspect the newly created builder
     docker buildx inspect --bootstrap "$BUILDER_NAME"
     
     echo "Builder '$BUILDER_NAME' created and set as the active builder."
 fi
-docker compose build turtlebot3-arm64 turtlebot3-x64 --progress=plain
-docker compose up -d turtlebot3-arm64 turtlebot3-x64
+docker compose build turtlebot3-arm64 --progress=plain
+docker compose up -d turtlebot3-arm64
 docker compose cp turtlebot3-arm64:/deb/ ./deb-arm64
+docker compose down
+
+# back to default build (assume its amd64/intel)
+docker buildx use default
+docker compose build turtlebot3-x64 --progress=plain
+docker compose up -d turtlebot3-x64 
 docker compose cp turtlebot3-x64:/deb/ ./deb-x64
 docker compose down
+
 # TODO: zip to create files?
